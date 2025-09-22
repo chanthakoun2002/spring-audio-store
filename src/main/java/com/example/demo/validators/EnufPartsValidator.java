cc98a92e93a972dff1470f1240c6567b04d7ae88
Andrew Chanthakoun <chanthakoun2002@gmail.com>
2025-09-21 19:51:06 -0700

    H: added error messaging for low inventory when updating/adding parts and products

--- src/main/java/com/example/demo/validators/EnufPartsValidator.java
+++ src/main/java/com/example/demo/validators/EnufPartsValidator.java
@@ -35,10 +35,46 @@ public class EnufPartsValidator implements ConstraintValidator<ValidEnufParts, P
             for (Part p : myProduct.getParts()) {
                 if (p.getInv()<(product.getInv()-myProduct.getInv()))return false;
             }
+
+            // this handles validation for parts of product when altering product will exceed bound of related parts
+            int diff = product.getInv() - myProduct.getInv();
+            if (diff <= 0) return true;
+            for (Part p : myProduct.getParts()) {
+                //checks each part for the product
+                Integer min = p.getMin();
+                int minVal = (min == null ? 0 : min);
+                int remaining = p.getInv() - diff;
+                // throw error if expected remaining exceeds minimum
+                if (remaining < minVal) {
+
+                    constraintValidatorContext.disableDefaultConstraintViolation();
+                    constraintValidatorContext
+                            .buildConstraintViolationWithTemplate(String.format("Not enough (%s) to above within minimum. Remaining would be %d (min is %d).", p.getName(), remaining, minVal))
+                            .addPropertyNode("inv")
+                            .addConstraintViolation();
+
+                    return false;
+                }
+            }
+
+
             return true;
         }
         else{
+            // handling for when creating a new product, dont let user add inventory when creating a new product
+            if (product.getInv() != 0) {
+
+                constraintValidatorContext.disableDefaultConstraintViolation();
+                constraintValidatorContext
+                        .buildConstraintViolationWithTemplate("Set inventory to 0 when creating a new product. Create empty product then update with parts and inventor.")
+                        .addPropertyNode("inv")
+                        .addConstraintViolation();
+                return false;
+            }
+
                 return true;
             }
+
+
     }
 }

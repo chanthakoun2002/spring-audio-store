1863dd6034c4f48b2c4c600ab739c529f86f9828
Andrew Chanthakoun <chanthakoun2002@gmail.com>
2025-09-23 21:06:31 -0700

    fixed product/part proper error message not appearing and added clearer wording

--- src/main/java/com/example/demo/validators/EnufPartsValidator.java
+++ src/main/java/com/example/demo/validators/EnufPartsValidator.java
@@ -28,50 +28,57 @@ public class EnufPartsValidator implements ConstraintValidator<ValidEnufParts, P
     @Override
     public boolean isValid(Product product, ConstraintValidatorContext constraintValidatorContext) {
         if(context==null) return true;
-        if(context!=null)myContext=context;
+        if(context!=null) myContext=context;
         ProductService repo = myContext.getBean(ProductServiceImpl.class);
         if (product.getId() != 0) {
             Product myProduct = repo.findById((int) product.getId());
-            for (Part p : myProduct.getParts()) {
-                if (p.getInv()<(product.getInv()-myProduct.getInv()))return false;
-            }
+//            for (Part p : myProduct.getParts()) {
+//                if (p.getInv()<(product.getInv()-myProduct.getInv()))return false;
+//            }
 
-            // this handles validation for parts of product when altering product will exceed bound of related parts
             int diff = product.getInv() - myProduct.getInv();
             if (diff <= 0) return true;
-            for (Part p : myProduct.getParts()) {
-                //checks each part for the product
-                Integer min = p.getMin();
-                int minVal = (min == null ? 0 : min);
-                int remaining = p.getInv() - diff;
-                // throw error if expected remaining exceeds minimum
-                if (remaining < minVal) {
 
-                    constraintValidatorContext.disableDefaultConstraintViolation();
-                    constraintValidatorContext
-                            .buildConstraintViolationWithTemplate(String.format("Not enough (%s) to above within minimum. Remaining would be %d (min is %d).", p.getName(), remaining, minVal))
-                            .addPropertyNode("inv")
-                            .addConstraintViolation();
-
-                    return false;
-                }
-            }
-
-
-            return true;
-        }
-        else{
-            // handling for when creating a new product, dont let user add inventory when creating a new product
-            if (product.getInv() != 0) {
-
+            // validation to make sure user adds parts before allowing adding more products
+            if (myProduct.getParts() == null || myProduct.getParts().isEmpty()) {
                 constraintValidatorContext.disableDefaultConstraintViolation();
                 constraintValidatorContext
-                        .buildConstraintViolationWithTemplate("Set inventory to 0 when creating a new product. Create empty product then update with parts and inventor.")
+                        .buildConstraintViolationWithTemplate("cannot add inventory until parts have been added")
                         .addPropertyNode("inv")
                         .addConstraintViolation();
                 return false;
             }
 
+            // this handles validation for parts of product when altering product will exceed bound of related parts
+            for (Part p : myProduct.getParts()) {
+                //checks each part for the product
+                Integer min = p.getMin();
+                int minVal = (min == null ? 0 : min);
+                int remaining = p.getInv() - diff;
+
+                // throw error if expected remaining exceeds minimum
+                if (remaining < minVal) {
+                    constraintValidatorContext.disableDefaultConstraintViolation();
+                    constraintValidatorContext
+                            .buildConstraintViolationWithTemplate(String.format("Not enough %s(s). Remaining would be %d.", p.getName(), remaining))
+                            .addPropertyNode("inv")
+                            .addConstraintViolation();
+                    return false;
+                }
+            }
+            return true;
+        }
+        else{
+            // new products should start with an inventory of 0
+            if (product.getInv() != 0) {
+
+                constraintValidatorContext.disableDefaultConstraintViolation();
+                constraintValidatorContext
+                        .buildConstraintViolationWithTemplate("When creating a new product, set inventory to 0. Create an empty product, then update to add parts and inventory.")
+                        .addPropertyNode("inv")
+                        .addConstraintViolation();
+                return false;
+            }
                 return true;
             }
 
